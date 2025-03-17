2a1263ec874e6580aa76fa8a5b4b78bc05c575ae
Manel082 <emmanuelhenrique43@gmail.com>
2025-03-17 17:24:47 -0300

    Update jogo de adivinhação.js

---
+++ aula2/jogo de adivinhação.js
@@ -0,0 +1,16 @@
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+let tentativas = 0;
+let palpite;
+
+do {
+    palpite = parseInt(prompt("Adivinhe o número entre 1 e 100:"));
+    tentativas++;
+
+    if (palpite < numeroAleatorio) {
+        console.log("O número é maior.");
+    } else if (palpite > numeroAleatorio) {
+        console.log("O número é menor.");
+    } else {
+        console.log("Parabéns! Você acertou o número em " + tentativas + " tentativas.");
+    }
+} while (palpite !== numeroAleatorio);
